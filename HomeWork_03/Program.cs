b1d844037ba008f990548afe882b515fe7c0d4e1
AlexanderDix <axdxrere@gmail.com>
2021-06-19 11:51:36 +0500

    Added input of the number of players and input of their names

--- HomeWork_03/Program.cs
+++ HomeWork_03/Program.cs
@@ -8,35 +8,49 @@ namespace HomeWork_03
         {
             // Создание переменной random для получения псевдослучайного числа
             Random random = new();
-
             // Создание переменной gameNumber для получения числа в диапазоне от 12 до 100
             var gameNumber = random.Next(12, 101);
 
+            // Просим ввести количество игроков, после чего создаем массив с нужным кол-вом
+            Console.WriteLine("Введите количество игроков");
+            var count = Convert.ToInt32(Console.ReadLine());
+            var userNames = new string[count];
+
+            // Просим ввести имена игроков
+            Console.WriteLine("Введите имена игроков");
+            for (int i = 0; i < count; i++)
+            {
+                userNames[i] = Console.ReadLine();
+            }
+
             while (gameNumber > 0)
             {
-                Console.WriteLine("Введите число от 1 до 4");
-                // Создание переменной userTry для пользовательского ввода
-                var userTry = Convert.ToInt32(Console.ReadLine()); 
-
-                switch (userTry)
+                foreach (var userName in userNames)
                 {
-                    case 1:
-                        gameNumber -= 1;
-                        break;
-                    case 2:
-                        gameNumber -= 2;
-                        break;
-                    case 3:
-                        gameNumber -= 3;
-                        break;
-                    case 4:
-                        gameNumber -= 4;
-                        break;
-                    default:
-                        continue;
-                }
+                    Console.WriteLine($"Игрок {userName}, введите число от 1 до 4");
+                    // Создание переменной userTry для пользовательского ввода
+                    var userTry = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine($"Число равно: {gameNumber}");
+                    switch (userTry)
+                    {
+                        case 1:
+                            gameNumber -= 1;
+                            break;
+                        case 2:
+                            gameNumber -= 2;
+                            break;
+                        case 3:
+                            gameNumber -= 3;
+                            break;
+                        case 4:
+                            gameNumber -= 4;
+                            break;
+                        default:
+                            continue;
+                    }
+
+                    Console.WriteLine($"Число равно: {gameNumber}");
+                }
             }
         }
     }
