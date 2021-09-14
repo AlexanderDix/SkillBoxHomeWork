b6c5e0745b3ab093c218e1a9911bdf9e7ff3b608
AlexanderDix <axdxrere@gmail.com>
2021-09-14 20:05:14 +0500

    Переименован класс. Добавлены методы Input - для ввода данных, CheckInput - для проверки введеных данных.

---
+++ HomeWork_07/HomeWork_07/InputOutput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HomeWork_07
+{
+    class InputOutput
+    {
+        public static void Text(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void Text(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static string Input(string header)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"{header}");
+            Console.ResetColor();
+
+            return Console.ReadLine();
+        }
+
+        public static int CheckInput()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var checkInput = int.TryParse(input, out var number);
+
+                if (checkInput && number >= 0) return number;
+
+                Text("Данные введены некорректно", ConsoleColor.DarkRed);
+            }
+        }
+    }
+}
