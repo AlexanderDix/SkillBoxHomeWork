03b025f1a5bb9b2c57a5c0b7338442c319c19efa
AlexanderDix <axdxrere@gmail.com>
2021-09-18 13:06:30 +0500

    Добавлены классы Employee и Departament

---
+++ HomeWork_08/HomeWork_08/Departament.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_08
+{
+    class Departament
+    {
+        #region Constructors
+
+        public Departament(string name, DateTime dateCreate)
+        {
+            Name = name;
+            DateCreate = dateCreate;
+            Employees = new List<Employee>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; set; }
+        public DateTime DateCreate { get; set; }
+        public List<Employee> Employees { get; set; }
+
+        #endregion
+    }
+}
---
+++ HomeWork_08/HomeWork_08/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWork_08
+{
+    class Employee
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string Departament { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Employee(int id, string firstName, string lastName, int age, int salary, string departament)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+            Departament = departament;
+        }
+
+        #endregion
+    }
+}
